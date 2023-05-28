78118079140bd4d41bdf6640434ec5793060ae20
Roman Konovalov <125948533+FreenF0X@users.noreply.github.com>
2023-05-28 19:43:36 +0500

    AddTestProject

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Шо то какой то текст на консоли. \nНажмите любую кнопку для завершения...");
+            Console.ReadLine();
+        }
+    }
+}
