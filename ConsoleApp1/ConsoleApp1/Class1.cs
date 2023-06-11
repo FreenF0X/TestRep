ec49fa2d12684cde3048269e6782275fd0f9a787
Roman Konovalov <125948533+FreenF0X@users.noreply.github.com>
2023-06-11 10:24:15 +0500

    newProject

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public Class1(Class2 class2) 
+        {
+            Class2 = class2;
+        }
+
+        public Class2 Class2 { get; }
+    }
+}
