0c7a2de5aaf531140f087fb6622012b89f1c391e
Roman Konovalov <125948533+FreenF0X@users.noreply.github.com>
2023-06-21 20:31:16 +0500

    newController

--- ConsoleApp1/ConsoleApp1/Controllers/HomeController.cs
+++ ConsoleApp1/ConsoleApp1/Controllers/HomeController.cs
@@ -4,9 +4,15 @@ namespace ConsoleApp1.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public string Index()
         {
             return "Hello, controllers.";
+        }
+        [HttpGet]
+        public string SomeMethod()
+        {
+            return "Some text";
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/Controllers/NewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1.Controllers
+{
+    public class NewController : Controller
+    {
+        [HttpGet]
+        public string NewMethod()
+        {
+            return "New text";
+        }
+    }
+}
