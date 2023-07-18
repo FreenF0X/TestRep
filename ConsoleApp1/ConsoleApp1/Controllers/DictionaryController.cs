184fb97b6a1493fda6e338878911a8f5eac22658
konovalov2k@mail.ru <konovalov2k@mail.ru>
2023-07-18 17:18:39 +0500

    добавлены контроллеры

---
+++ ConsoleApp1/ConsoleApp1/Controllers/DictionaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Controllers/ListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
